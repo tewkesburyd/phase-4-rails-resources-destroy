87f583f672ad6bf1d080215094c79ed38d8fa10a
David Tewkesbury <tewkesburyd@hotmail.com>
2023-01-11 13:49:42 -0700

    commit tree 2045b35e551c8306a47a0f5ec9d1c2393c3b8489

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -33,6 +33,16 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   # PATCH /birds/:id/like
   def increment_likes
     bird = Bird.find_by(id: params[:id])
